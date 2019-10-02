b9e788d2c2f154aad2fdd397980f21aa0696874d
Jirawat Harnsiriwatanakit <jirawat.h@inception.asia>
2019-10-02 09:53:34 +0700

    add alignment interface

---
+++ enum/alignment/withhorizontal.go
@@ -0,0 +1,9 @@
+/*
+  GoLang code created by Jirawat Harnsiriwatanakit https://github.com/kazekim
+*/
+
+package alignment
+
+type WithHorizontalAlignment interface {
+	setHorizontalAlignment(alignment HorizontalAlignment)
+}
---
+++ enum/alignment/withvertical.go
@@ -0,0 +1,10 @@
+/*
+  GoLang code created by Jirawat Harnsiriwatanakit https://github.com/kazekim
+*/
+
+package alignment
+
+type WithVerticalAlignment interface {
+	setVerticalAlignment(alignment VerticalAlignment)
+}
+
