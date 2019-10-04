f4165a96f1353a64b8070a334a4f4d1f648a52f7
Jirawat Harnsiriwatanakit <jirawat.h@inception.asia>
2019-10-04 15:55:52 +0700

    add bad password error

---
+++ error/badpassword.go
@@ -0,0 +1,21 @@
+/*
+  GoLang code created by Jirawat Harnsiriwatanakit https://github.com/kazekim
+*/
+
+package error
+
+const badPasswordSerialVersionUID = -4333706268155063964
+
+type BadPasswordError struct {
+	message string
+}
+
+func NewBadPasswordError(message string) error {
+	return &BadPasswordError{
+		message: message,
+	}
+}
+
+func (e BadPasswordError) Error() string {
+	return e.message
+}
